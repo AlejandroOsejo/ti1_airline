5cccf678edd84b16fa357d692c1ed2d64eca9bd2
Skydoes10 <ricardoms1909@gmail.com>
2023-04-22 18:09:41 -0500

    Mode FXML adde and BoardingGUI created

---
+++ src/ui/BoardingGUI.java
@@ -0,0 +1,35 @@
+package ui;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import model.BoardingSystem;
+
+public class BoardingGUI {
+
+    // Mode Pane
+    @FXML
+    private BorderPane bpMode;
+
+    @FXML
+    private Button btnNormal;
+
+    @FXML
+    private Button btnTest;
+
+    public BoardingGUI(BoardingSystem boardingSystem) {
+
+    }
+
+    @FXML
+    private void handleNormal() {
+        // TODO
+    }
+
+    @FXML
+    private void handleTest() {
+        // TODO
+    }
+
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,18 @@
 package ui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.BoardingSystem;
 
-public class Main {
+public class Main extends Application {
+    private BoardingSystem boardingSystem;
+    private BoardingGUI boardingGUI;
     public static void main(String[] args) {
-        BoardingSystem boardingSystem = new BoardingSystem("passangers.txt");
+        launch(args);
+        /*BoardingSystem boardingSystem = new BoardingSystem("passangers.txt");
 
         System.out.println("Passangers:");
         boardingSystem.printPassengers();
@@ -23,7 +31,23 @@ public class Main {
         boardingSystem.setPriorityEntrance();
 
         System.out.println("\nBoarding queue:");
-        boardingSystem.printBoardingQueue();
+        boardingSystem.printBoardingQueue();*/
+    }
 
+    public Main() {
+        boardingSystem = new BoardingSystem("passangers.txt");
+        boardingGUI = new BoardingGUI(boardingSystem);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Mode.fxml"));
+        loader.setController(boardingGUI);
+
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Boarding System");
+        primaryStage.show();
     }
 }
