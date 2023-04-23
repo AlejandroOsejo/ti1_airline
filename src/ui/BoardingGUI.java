4a363cf2c5c72b3138efc83745e47ee888af8c7d
Skydoes10 <ricardoms1909@gmail.com>
2023-04-22 21:16:09 -0500

    handleNormal and handleTest methods implemented. Some things changed in fxml files

--- src/ui/BoardingGUI.java
+++ src/ui/BoardingGUI.java
@@ -2,9 +2,15 @@ package ui;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import model.BoardingSystem;
+
+import java.io.IOException;
 
 public class BoardingGUI {
 
@@ -18,18 +24,50 @@ public class BoardingGUI {
     @FXML
     private Button btnTest;
 
+    // Main Pane
+    @FXML
+    private Button btnLoad;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnViewBoarding;
+
+    @FXML
+    private Button btnViewExit;
+
+
     public BoardingGUI(BoardingSystem boardingSystem) {
 
     }
 
     @FXML
-    private void handleNormal() {
-        // TODO
+    private void handleNormal() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        loader.setController(this);
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Boarding System");
+        stage.show();
+
+        Stage stage2 = (Stage) btnNormal.getScene().getWindow();
+        stage2.close();
     }
 
     @FXML
-    private void handleTest() {
-        // TODO
+    private void handleTest() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        loader.setController(this);
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Boarding System");
+        stage.show();
+
+        Stage stage2 = (Stage) btnTest.getScene().getWindow();
+        stage2.close();
     }
 
 }
